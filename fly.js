bcfc9ace09b85b8bc5d0dbf83b5fbb08e4b8aee2
andythecoderman <abatchelor@tritondigital.com>
2013-06-13 22:28:47 -0400

    added some more bootstrapping

--- fly.js
+++ fly.js
@@ -5,3 +5,20 @@
  * Time: 9:55 PM
  * To change this template use File | Settings | File Templates.
  */
+
+var arDrone = require('ar-drone');
+var client = arDrone.createClient();
+
+client.takeoff();
+
+client
+    .after(5000, function() {
+        this.clockwise(0.5);
+    })
+    .after(3000, function() {
+        //this.animate('flipLeft', 15);
+    })
+    .after(1000, function() {
+        this.stop();
+        this.land();
+    });
